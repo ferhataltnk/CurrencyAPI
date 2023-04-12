4a4ca82802aa79f40751b81157a9233030f18980
ferhat <STJ79730@NB317493>
2023-04-12 21:15:06 +0300

    Serilog Configured

--- Business/Services/Concrete/CurrencyManager.cs
+++ Business/Services/Concrete/CurrencyManager.cs
@@ -2,7 +2,7 @@
 using Core.Utilities.Results;
 using DataAccess.Services.Abstract;
 using Entities;
-using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,10 +14,10 @@ namespace Business.Services.Concrete
 {
     public class CurrencyManager : ICurrencyService
     {
-        private readonly ILogger<CurrencyManager> _logger;
+        private readonly ILogger _logger;
         private readonly ICurrencyDal _currencyDal;
 
-        public CurrencyManager(ICurrencyDal currencyDal, ILogger<CurrencyManager> logger)
+        public CurrencyManager(ICurrencyDal currencyDal, ILogger logger)
         {
             _currencyDal = currencyDal;
             _logger = logger;
@@ -29,14 +29,14 @@ namespace Business.Services.Concrete
             try
             {
                 var valueResult = _currencyDal.GetCurrenciesBetweenDates(dateStart, dateEnd);
-                _logger.LogInformation(valueResult.Message);
+                _logger.Information(valueResult.Message);
                
                 return new Result<List<Currency>>(data: valueResult.Data, message: $"İstenilen tarih aralığındaki kur değerleri başarıyla getirildi.{Environment.NewLine}Detay:{valueResult.Message}", success:true);
             }
 
             catch (Exception ex)
             {
-                _logger.LogError($"{GetType().FullName}.{MethodBase.GetCurrentMethod()?.Name}.Failed.{Environment.NewLine}İki tarih arasındaki kur değerleri getirilirken bir hata oluştu.{Environment.NewLine}Detay:{ex.Message}");
+                _logger.Error($"{GetType().FullName}.{MethodBase.GetCurrentMethod()?.Name}.Failed.{Environment.NewLine}İki tarih arasındaki kur değerleri getirilirken bir hata oluştu.{Environment.NewLine}Detay:{ex.Message}");
               
                 return new Result<List<Currency>>(data: null, message:$"İki tarih arasındaki kur değerleri getirilirken bir hata oluştu.{Environment.NewLine}Detay:{ ex.Message }",success:false);
                 
@@ -50,13 +50,13 @@ namespace Business.Services.Concrete
             try
             {
                 var valueResult = _currencyDal.GetCurrenciesByCode(currencyCode);
-                _logger.LogInformation(valueResult.Message);
+                _logger.Information(valueResult.Message);
                 return new Result<Currency>(data: valueResult.Data, message: $"Belirtilen kur değeri başarıyla getirildi.{Environment.NewLine}Detay:{valueResult.Message }", success: true);
             }
 
             catch (Exception ex)
             {
-                _logger.LogError(ex.Message);
+                _logger.Error(ex.Message);
                 return new Result<Currency>(data: null, message: $"Belirtilen kur değeri getirilirken bir hata oluştu.{Environment.NewLine}Detay:{ex.Message}", success: false);
 
             }
@@ -70,13 +70,13 @@ namespace Business.Services.Concrete
             try
             {
                 var valueResult = _currencyDal.GetCurrenciesByCodeAndBetweenDates(currencyCode: currencyCode, dateStart: dateStart, dateEnd: dateEnd);
-                _logger.LogInformation(valueResult.Message);
+                _logger.Information(valueResult.Message);
                 return new Result<List<Currency>>(data: valueResult.Data, message: $"İstenilen tarih aralığındaki belirtilen kur değeri başarıyla getirildi.{Environment.NewLine}Detay:{valueResult.Message}", success: true);
             }
 
             catch (Exception ex)
             {
-                _logger.LogError(ex.Message);
+                _logger.Error(ex.Message);
                 return new Result<List<Currency>>(data: null, message: $"İki tarih arasında belirtilen kur değerleri getirilirken bir hata oluştu.{Environment.NewLine}Detay:{ex.Message}", success: false);
 
             }
--- CurrencyAPI/Program.cs
+++ CurrencyAPI/Program.cs
@@ -2,11 +2,24 @@ using Business.Services.Abstract;
 using Business.Services.Concrete;
 using DataAccess.Services.Abstract;
 using DataAccess.Services.Concrete;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+//Serilog Configuration
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Debug()
+    .CreateLogger();
+//Serilog IOC
+builder.Host.UseSerilog(Log.Logger);
+
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -14,6 +27,9 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<ICurrencyService,CurrencyManager>();
 builder.Services.AddSingleton<ICurrencyDal,DpCurrencyDal>();
+
+
+
 
 var app = builder.Build();
 
